3e80c94a6f82464811a0006fc77cd25d6a96a8e8
SahinMuhammetAbdullah <m.sahingithub@gmail.com>
2022-10-31 20:00:10 +0300

    main

--- datatypes/src/App.java
+++ datatypes/src/App.java
@@ -2,9 +2,11 @@ public class App {
     public static void main(String[] args) throws Exception {
         int sayi = 12;
         sayi = 13;
+
         byte sayi1;
         sayi1 = -128;
         sayi1 = 127;
+        
         double sayi2 = 3.4;
 
         char karakter = 'a';
