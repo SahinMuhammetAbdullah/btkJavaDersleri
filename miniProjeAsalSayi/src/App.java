5bcd635dd09ae9d453c7fcf840e9cb16c729b976
SahinMuhammetAbdullah <m.sahingithub@gmail.com>
2022-11-22 17:30:27 +0300

    main

--- miniProjeAsalSayi/src/App.java
+++ miniProjeAsalSayi/src/App.java
@@ -1,5 +1,5 @@
 public class App {
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) {
         /*int sayi = 2 ;
         if (sayi%2 != 0 && sayi%3 != 0 && sayi%5 != 0 && sayi%7 != 0 && sayi%11 != 0 ) {
             System.out.println(sayi + " " + "sayisi asal");
@@ -8,7 +8,7 @@ public class App {
             System.out.println(sayi + " " + "sayisi asal degil");
         }*/
 
-        int number = 0;
+        int number = 5;
         boolean isPrime = true;
 
         if (number == 1) {
